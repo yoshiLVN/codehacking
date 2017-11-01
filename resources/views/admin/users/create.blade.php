c237ea693f195c8fa19297291e643fce65b65ef5
yoshi <yoshi@lvnetworks.com>
2017-11-01 16:46:10 -0400

    users able to register with photos

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -31,8 +31,8 @@
     </div>
 
     <div class="form-group">
-            {!! Form::label('file', 'File') !!}
-            {!! Form::file('file',null, ['class'=>'form-control']) !!}
+            {!! Form::label('photo_id', 'Photo') !!}
+            {!! Form::file('photo_id',null, ['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
