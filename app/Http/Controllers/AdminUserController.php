12f910ea3bec16b586b84a2500c1dbd6891a5cf3
yoshi <yoshi@lvnetworks.com>
2017-11-01 15:27:16 -0400

    working form with error fields

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -30,7 +32,10 @@ class AdminUserController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+        $roles = Role::lists('name','id')->all();
+//        return $roles;
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -39,8 +44,10 @@ class AdminUserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
+        return $request->all();
+
         //
     }
 
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -13,11 +13,33 @@
         {!! Form::label('name', 'Name') !!}
         {!! Form::text('name', null, ['class'=>'form-control']) !!}
     </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email') !!}
+        {!! Form::text('email',null, ['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role') !!}
+         {!! Form::select('role_id',[''=>'Choose Options'] + $roles ,null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+            {!! Form::label('status', 'Status') !!}
+            {!! Form::select('status',array(1 =>'Active', 0=>'Not Active'),0, ['class'=>'form-control']) !!}
+        </div>
+    <div class="form-group">
+            {!! Form::label('password', 'Password') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
     <div class="form-group">
         {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
     </div>
 
     {!! Form::close() !!}
-    
+
+    @include('includes.form_error')
     
 @endsection
